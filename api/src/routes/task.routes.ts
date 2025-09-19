7747e29f45355f89b98f340429a634d2f0415ae0
matheustorresdev97 <matheustorresdev@gmail.com>
2025-09-19 08:52:05 -0300

    task routes

--- api/src/routes/task.routes.ts
+++ api/src/routes/task.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { TaskController } from "../controller/task.controller";
 
 const taskRoutes = Router();
+const taskController = new TaskController();
 
+taskRoutes.post('/', taskController.create);
 export { taskRoutes };
