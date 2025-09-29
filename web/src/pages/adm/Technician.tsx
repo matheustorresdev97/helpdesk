127028eecea74a14c94618c901a4590d1caf195d
matheustorresdev97 <matheustorresdev@gmail.com>
2025-09-29 18:38:12 -0300

    client index / update layout and dtos

--- web/src/pages/adm/Technician.tsx
+++ web/src/pages/adm/Technician.tsx
@@ -53,7 +53,7 @@ export function Technician() {
   }, [page]);
   return (
     <>
-      <div className="flex place-content-between mb-7">
+      <div className="flex place-content-between mb-7 items-center">
         <h1 className="text-blue-dark font-lato font-bold text-2xl">
           Técnicos
         </h1>
